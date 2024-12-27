8720818ad7ed4830402cdb05fd8d15cf0f47bb95
DanielOpara <oparadaniv@gmail.com>
2024-12-27 16:31:02 +0100

    added login functionality

--- book-management-app/app/login/page.tsx
+++ book-management-app/app/login/page.tsx
@@ -1,9 +1,152 @@
-import React from "react";
+"use client";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import { toast } from "react-toastify"; // Add toast import
+
+interface FormData {
+  email: string;
+  password: string;
+}
+
+interface ApiResponse {
+  statusCode: number;
+  description: string;
+  message: string;
+  success: boolean;
+  error: string;
+}
 
 const Login = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    // Form validation
+    if (!formData.email || !formData.password) {
+      toast.error("Please fill in all fields.");
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const payload = {
+        email: formData.email,
+        password: formData.password,
+      };
+
+      const response = await fetch("http://localhost:3001/api/v1/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+
+      const data: ApiResponse = await response.json();
+
+      if (data.statusCode === 200) {
+        console.log(data);
+        toast.success("Login successful!");
+      } else {
+        toast.error(data.error, {
+          autoClose: false,
+          closeOnClick: true,
+        });
+      }
+    } catch (error) {
+      toast.error("Something went wrong. Please try again.");
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1>LOGIN</h1>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
+        <h2 className="text-2xl font-semibold text-center text-gray-700">
+          Login
+        </h2>
+        <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              required
+            />
+          </div>
+
+          <div className="relative">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Password
+            </label>
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="mt-1 block w-full px-4 py-2 pr-10 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              required
+            />
+            <span
+              className="absolute top-11 right-3 transform -translate-y-1/2 text-xl text-gray-600 cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+            </span>
+          </div>
+
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="w-full px-4 py-2 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              disabled={isLoading}
+            >
+              {isLoading ? "Loading..." : "Login"}
+            </button>
+          </div>
+        </form>
+        <p className="mt-4 text-center text-sm text-gray-600">
+          Do not have an account?{" "}
+          <a href="/register" className="text-indigo-600 hover:text-indigo-700">
+            Register
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
